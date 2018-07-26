ed08f5dfa2bb4a68163519587b846675686fd9dc
Marcel Groeneweg <marcel.groeneweg@itvisors.com>
2018-07-26 13:45:19 +0200

    Now with options for playback and starting a new recording.

--- src/AudioRecorderWidget/widget/AudioRecorderWidget.js
+++ src/AudioRecorderWidget/widget/AudioRecorderWidget.js
@@ -1,6 +1,6 @@
 /*jshint undef: true, browser:true, nomen: true */
 /*jslint browser:true, nomen: true */
-/*global mx, mxui, define, require, console, cordova, logger, resolveLocalFileSystemURL, CaptureError */
+/*global mx, mxui, define, require, console, cordova, logger, resolveLocalFileSystemURL, CaptureError, Media */
 /*
 
 
@@ -13,9 +13,11 @@ define([
     "dojo/_base/declare",
     "mxui/widget/_WidgetBase",
     "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/on",
     "dojo/_base/lang"
 
-], function (declare, _WidgetBase, dojoStyle, lang) {
+], function (declare, _WidgetBase, dojoStyle, dojoConstruct, dojoOn, lang) {
     "use strict";
 
     return declare("AudioRecorderWidget.widget.AudioRecorderWidget", [ _WidgetBase ], {
@@ -24,12 +26,33 @@ define([
 
         // Internal variables.
         _contextObj: null,
+        _playButton: null,
+        _stopButton: null,
+        _recordButton: null,
+        _recordingPath: null,
+        _media: null,
 
         constructor: function () {
         },
 
         postCreate: function () {
             logger.debug(this.id + ".postCreate");
+            this._playButton = this._placeButtonWithIcon("play");
+            this._stopButton = this._placeButtonWithIcon("stop");
+            this._recordButton = this._placeButtonWithIcon("record");
+            dojoOn(this._playButton, "click", lang.hitch(this, this._handlePlayButtonClick));
+            dojoOn(this._stopButton, "click", lang.hitch(this, this._handleStopButtonClick));
+            dojoOn(this._recordButton, "click", lang.hitch(this, this._handleRecordButtonClick));
+        },
+
+        _placeButtonWithIcon: function (glyphiconName) {
+            var buttonHtml;
+
+            buttonHtml =
+                "<button type='button' class='btn mx-button btn-default'><span class='glyphicon glyphicon-" + 
+                glyphiconName + 
+                "'></span></button>";
+            return dojoConstruct.place(buttonHtml, this.domNode);
         },
 
         update: function (obj, callback) {
@@ -41,6 +64,45 @@ define([
 
         uninitialize: function () {
             logger.debug(this.id + ".uninitialize");
+        },
+
+        _handlePlayButtonClick: function () {
+            logger.debug(this.id + "._handlePlayButtonClick");
+            var thisObj = this;
+            if (!this._media) {
+                console.log("Start media playback");
+                this._media = new Media(this._recordingPath, function(e) {
+                    thisObj._media.release();
+                    thisObj._media = null;
+                    console.log("Media playback stopped, released media object");
+                }, function (err) {
+                    console.log("media playback error: ", err);
+                }, function (statusCode) {
+                    console.log("media playback status: " + statusCode);
+                });
+                this._media.play();
+            }
+        },
+
+        _handleStopButtonClick: function () {
+            logger.debug(this.id + "._handleStopButtonClick");
+            this._stopPlayback();
+        },
+
+        _stopPlayback: function () {
+            logger.debug(this.id + "._stopPlayback");
+            console.log("Stop media playback");
+            if (this._media) {
+                this._media.stop();
+                this._media.release();
+                this._media = null;
+            }
+        },
+
+        _handleRecordButtonClick: function () {
+            logger.debug(this.id + "._handleRecordButtonClick");
+            this._stopPlayback();
+            this._startRecording();            
         },
 
         _updateRendering: function (callback) {
@@ -57,7 +119,7 @@ define([
         },
 
         _startRecording: function () {
-
+            console.log("Start new recording");
             if (typeof navigator.device.capture.captureAudio === "undefined") {
                 mx.ui.error("Audio device not detected.");
                 return;
@@ -68,14 +130,14 @@ define([
         },
 
         _captureSuccess: function (mediaFiles) {
-            var path,
-                thisObj = this;
+            var thisObj = this;
 
             // Result is always an array, if recording succeeded, array has one element.
             if (mediaFiles.length) {
-                path = mediaFiles[0].localURL;
+                this._recordingPath = mediaFiles[0].localURL;
                 // Resolve path into a file entry
-                resolveLocalFileSystemURL(path, function (fileEntry) {
+                resolveLocalFileSystemURL(this._recordingPath, function (fileEntry) {
+                    thisObj._contextObj.set("Name", fileEntry.name);
                     // A file retrieved using resolveLocalFileSystemURL can be stored directly in Mendix because File inherits from Blob.
                     fileEntry.file(function(file) {
                         window.mx.data.saveDocument(
@@ -97,7 +159,7 @@ define([
 
         _saveDocumentCallback: function (path) {
             logger.debug(this.id + "._saveDocumentCallback");
-            console.log("Document saved from path " + path);
+            //console.log("Document saved from path " + path);
         },
 
         _showError: function (e) {
